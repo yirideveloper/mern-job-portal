8a07943efa070aae9c5dee989f81237be556917a
yirideveloper <topyiri1113@gmail.com>
2018-11-24 23:51:11 -0800

    Add skills in search jobs filter

--- backend/src/routes/api/index.js
+++ backend/src/routes/api/index.js
@@ -13,7 +13,6 @@ const documentRouter = require('./document.route')
 router.get('/status', (req, res) => { res.send({status: 'OK'}) }) // api status
 
 router.use('/auth', authRouter)
-
 router.use('/users', usersRouter)
 router.use('/jobs', jobsRouter)
 router.use('/search', searchRouter)
