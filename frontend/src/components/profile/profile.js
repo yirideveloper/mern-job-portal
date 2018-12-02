0b62ae9a028816e7a5dc4bb414af64411e9f545e
yirideveloper <topyiri1113@gmail.com>
2018-12-02 15:06:04 -0800

    Fixed recommendation count issue

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -1209,7 +1209,7 @@ class profile extends Component {
                         
                                                     <div className="col-lg-3 right-sidebar">
                                                         <div >
-                                                            <a href={`/public-profile/${this.state.publicid}`}  className="view-public save-button">View Public Page</a>
+                                                            <a href="javascript:void(0)" className="view-public save-button"><Link to={`/public-profile/${this.state.publicid}`} target="_blank" >View Public Page</Link> </a>
                                                         </div>
                                                         <div className="widget widget-portfolio">
                                                             <div className="wd-heady">
